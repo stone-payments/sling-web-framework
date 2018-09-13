5774981dbb07cde2f203208cfe17c9d926492aa1
biancarosa <bmendonca@stone.com.br>
2018-09-13 20:36:15 -0300

    feat: add sling-assets

---
+++ packages/sling-assets/README.md
@@ -0,0 +1,3 @@
+# sling-assets
+
+Describe sling-assets here.
---
+++ packages/sling-assets/src/icons/index.js
@@ -0,0 +1,13 @@
+import { CANCEL } from './js/baseline-cancel-24px.js';
+import { CHECK_CIRCLE } from './js/baseline-check_circle-24px.js';
+import { CLOSE } from './js/baseline-close-24px.js';
+import { ERROR } from './js/baseline-error-24px.js';
+import { FEEDBACK } from './js/baseline-feedback-24px.js';
+
+export {
+  CANCEL,
+  CHECK_CIRCLE,
+  CLOSE,
+  ERROR,
+  FEEDBACK,
+};
