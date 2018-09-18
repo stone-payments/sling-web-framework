b2de6ddb38977560b721138b59feb6fd3a0aff50
Caio Matheus Veloso Dias <cmdias@stone.com.br>
2018-09-18 15:42:30 -0300

    :white_check_mark: Add tests to brand icon component.

--- packages/sling-web-component-brand-icon/src/component/BrandIcon.test.js
+++ packages/sling-web-component-brand-icon/src/component/BrandIcon.test.js
@@ -1,4 +1,65 @@
 /* eslint-disable */
+import { domHelper } from 'sling-helpers';
 import { BrandIcon } from './BrandIcon.js';
 
-// TODO Write me!
+domHelper.registerComponent('sling-brand-icon', BrandIcon);
+
+let $brandIcon
+
+describe('Brand Icon', () => {
+  beforeEach(() => {
+    $brandIcon = document.createElement('sling-brand-icon');
+    document.body.appendChild($brandIcon);
+  });
+
+  afterEach(() => {
+    document.body.removeChild($brandIcon);
+    $brandIcon = undefined;
+  });
+
+  it('Should reflect "brandid" attribute to property ', () => {
+    $brandIcon.setAttribute('brandid', '2');
+    setTimeout(() => {
+      expect($brandIcon.brandid).to.equal(2)
+    })
+  })
+
+  it('Should reflect "width" attribute to property ', () => {
+    $brandIcon.setAttribute('width', '50');
+    setTimeout(() => {
+      expect($brandIcon.width).to.equal(50)
+    })
+  })
+
+  it('Should reflect "height" attribute to property ', () => {
+    $brandIcon.setAttribute('height', '50');
+    setTimeout(() => {
+      expect($brandIcon.height).to.equal(50)
+    })
+  })
+
+  it('Should reflect "brandid" property to attribute ', () => {
+    $brandIcon.brandid = 3;
+
+    setTimeout(() => {
+      expect($brandIcon.getAttribute('brandid')).to.equal('3')
+    })
+  })
+
+  it('Should reflect "width" property to attribute ', () => {
+    $brandIcon.width = 50;
+
+    setTimeout(() => {
+      expect($brandIcon.getAttribute('width')).to.equal('50')
+    })
+  })
+
+  it('Should reflect "height" property to attribute ', () => {
+    $brandIcon.height = 50;
+
+    setTimeout(() => {
+      expect($brandIcon.getAttribute('height')).to.equal('50')
+    })
+  })
+});
+
