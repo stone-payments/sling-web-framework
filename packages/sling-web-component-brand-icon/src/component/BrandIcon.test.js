0bd818a3fb9a3fa0fa2684ff498ecfc9b5cb216b
Caio Matheus Veloso Dias <cmdias@stone.com.br>
2018-09-18 18:02:02 -0300

    :white_check_mark: Update Tests.

--- packages/sling-web-component-brand-icon/src/component/BrandIcon.test.js
+++ packages/sling-web-component-brand-icon/src/component/BrandIcon.test.js
@@ -17,49 +17,24 @@ describe('Brand Icon', () => {
     $brandIcon = undefined;
   });
 
-  it('Should reflect "brandid" attribute to property ', () => {
+  it('Should reflect "brandid", "width" and "heigth" attribute to property ', () => {
     $brandIcon.setAttribute('brandid', '2');
-    setTimeout(() => {
-      expect($brandIcon.brandid).to.equal(2)
-    })
+    $brandIcon.setAttribute('width', 50);
+    $brandIcon.setAttribute('height', 50);
+
+    expect($brandIcon.brandid).to.equal(2)
+    expect($brandIcon.width).to.equal('50')
+    expect($brandIcon.height).to.equal('50')
   })
 
-  it('Should reflect "width" attribute to property ', () => {
-    $brandIcon.setAttribute('width', '50');
-    setTimeout(() => {
-      expect($brandIcon.width).to.equal(50)
-    })
-  })
-
-  it('Should reflect "height" attribute to property ', () => {
-    $brandIcon.setAttribute('height', '50');
-    setTimeout(() => {
-      expect($brandIcon.height).to.equal(50)
-    })
-  })
-
-  it('Should reflect "brandid" property to attribute ', () => {
+  it('Should reflect "brandid", "width" and "heigth" property to attribute ', () => {
     $brandIcon.brandid = 3;
-
-    setTimeout(() => {
-      expect($brandIcon.getAttribute('brandid')).to.equal('3')
-    })
-  })
-
-  it('Should reflect "width" property to attribute ', () => {
     $brandIcon.width = 50;
-
-    setTimeout(() => {
-      expect($brandIcon.getAttribute('width')).to.equal('50')
-    })
-  })
-
-  it('Should reflect "height" property to attribute ', () => {
     $brandIcon.height = 50;
 
-    setTimeout(() => {
-      expect($brandIcon.getAttribute('height')).to.equal('50')
-    })
+    expect($brandIcon.getAttribute('brandid')).to.equal('3')
+    expect($brandIcon.getAttribute('width')).to.equal('50')
+    expect($brandIcon.getAttribute('height')).to.equal('50')
   })
 });
 
