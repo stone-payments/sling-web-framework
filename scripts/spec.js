0e2cb01baaaeba653830e928007dea387caca682
Caio Matheus Veloso Dias <cmdias@stone.com.br>
2018-09-19 14:33:55 -0300

    Update script spec

--- scripts/spec.js
+++ scripts/spec.js
@@ -1,4 +1,5 @@
 const { exec, env } = require('shelljs');
+const { join } = require('path');
 
 const getScope = require('./helpers/getScope');
 const getBasePath = require('./helpers/getBasePath');
@@ -7,10 +8,11 @@ const scope = getScope();
 console.log(`Testing ${scope !== '*' ? scope : 'all packages'}\n`);
 
 const basePath = getBasePath(scope);
+const testFiles = join(basePath, 'src', '**', '*.spec.js');
+const nyc = join('node_modules', '.bin', 'nyc');
+const mochaWebpack = join('node_modules', '.bin', 'mocha-webpack');
 
 env.PKG_SCOPE = scope;
 
-exec(`${scope === '*' ? './node_modules/.bin/nyc ' : ''}` +
-  './node_modules/.bin/mocha-webpack --colors ' +
-  '--webpack-config webpack.spec.config.js ' +
-  `"${basePath}/src/**/*.spec.js"`);
+exec(`${scope === '*' ? `${nyc} ` : ''}` +
+  `${mochaWebpack} --colors --webpack-config webpack.spec.config.js "${testFiles}"`);
