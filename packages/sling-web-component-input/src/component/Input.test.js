cca56325df7f944a53fc7c3e9655612b6735cb72
Joselito Nascimento | Stone <joselito.nascimento@stone.com.br>
2018-09-21 10:31:32 -0300

    Updates tests

--- packages/sling-web-component-input/src/component/Input.test.js
+++ packages/sling-web-component-input/src/component/Input.test.js
@@ -1,4 +1,4 @@
-import { domHelper } from '../../node_modules/sling-web-helpers/src/index.js';
+import { domHelper } from 'sling-web-helpers';
 import { Input } from './Input.js';
 
 domHelper.registerComponent('sling-input', Input);
