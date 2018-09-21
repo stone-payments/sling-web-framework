9c34660f658034271388e63b54384bcf3763535a
leofavre <contato@leofavre.com>
2018-09-21 17:15:35 -0300

    Fix imports

--- packages/sling-web-component-form/src/index.js
+++ packages/sling-web-component-form/src/index.js
@@ -1,4 +1,4 @@
+import { registerComponent } from 'sling-helpers';
 import { Form } from './component/Form.js';
-import { domHelper } from '../node_modules/sling-web-helpers/src/index.js';
 
-domHelper.registerComponent('sling-form', Form);
+registerComponent('sling-form', Form);
