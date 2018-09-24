c8799bc21d06288e058b315e66bcaa2b93b189aa
Leonardo Favre | Stone <leonardo.favre@stone.com.br>
2018-09-24 15:49:04 -0300

    Move npmrc to config folder

--- scripts/spec.js
+++ scripts/spec.js
@@ -10,9 +10,10 @@ console.log(`Testing ${scope !== '*' ? scope : 'all packages'}\n`);
 const basePath = getBasePath(scope);
 const testFiles = join(basePath, 'src/**/*.spec.js');
 const nyc = join('node_modules/.bin/nyc');
+const nycRc = join('scripts/config/.nycrc');
 const mochaWebpack = join('node_modules/.bin/mocha-webpack');
 
 env.PKG = scope;
 
-exec(`${scope === '*' ? `${nyc} ` : ''}` +
+exec(`${scope === '*' ? `${nyc} --nycrc-path ${nycRc} ` : ''}` +
   `${mochaWebpack} --colors --webpack-config webpack.spec.config.js "${testFiles}"`);
