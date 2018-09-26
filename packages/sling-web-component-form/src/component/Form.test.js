c97510b01963636e9579afeaf4c298ae4cc0d6ae
Joselito Nascimento | Stone <joselito.nascimento@stone.com.br>
2018-09-26 20:43:04 -0300

    Init Form Tests

--- packages/sling-web-component-form/src/component/Form.test.js
+++ packages/sling-web-component-form/src/component/Form.test.js
@@ -1,4 +1,24 @@
-/* eslint-disable */
+import { registerComponent } from 'sling-helpers';
 import { Form } from './Form.js';
 
-// TODO Write me!
+registerComponent('sling-form', Form);
+
+let formElement;
+
+describe('Form', () => {
+  beforeEach(() => {
+    formElement = document.createElement('sling-form');
+    document.body.appendChild(formElement);
+  });
+
+  afterEach(() => {
+    if (formElement != null) {
+      document.body.removeChild(formElement);
+      formElement = undefined;
+    }
+  });
+
+  it('Should create the element', () => {
+    expect(formElement).not.to.equal(undefined);
+  });
+});
