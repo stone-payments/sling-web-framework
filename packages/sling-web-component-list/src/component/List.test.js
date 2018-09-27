a26e7aa8f4fc63a65006a95323968bef80411815
Caio Matheus Veloso Dias <cmdias@stone.com.br>
2018-09-27 16:38:33 -0300

    :white_check_mark: Add tests.

--- packages/sling-web-component-list/src/component/List.test.js
+++ packages/sling-web-component-list/src/component/List.test.js
@@ -1,4 +1,32 @@
-/* eslint-disable */
+import { registerComponent } from 'sling-helpers';
 import { List } from './List.js';
 
-// TODO Write me!
+registerComponent('sling-list', List);
+
+let $List;
+
+describe('List', () => {
+  beforeEach(() => {
+    $List = document.createElement('sling-list');
+    document.body.appendChild($List);
+  });
+
+  afterEach(() => {
+    if ($List != null) {
+      document.body.removeChild($List);
+      $List = undefined;
+    }
+  });
+
+  it('Should reflect "cascadelist" to attribute to property', () => {
+    $List.setAttribute('cascadelist', '');
+
+    expect($List.cascadelist).to.be.true;
+  });
+
+  it('Should reflect "cascadelist" to attribute to property', () => {
+    $List.cascadelist = false;
+
+    expect($List.cascadelist).to.be.false;
+  });
+});
