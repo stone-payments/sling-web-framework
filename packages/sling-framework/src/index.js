462a890b77f0bae4c2db68e999ec82e069b68143
Vitor Fernandes Loureiro Santos <vloureiro@stone.com.br>
2018-10-04 16:04:53 -0300

    adds exporting for withRequestParams

--- packages/sling-framework/src/index.js
+++ packages/sling-framework/src/index.js
@@ -2,3 +2,4 @@ export { html } from './lib/lit-element.bundle.js';
 export { SlingElement } from './framework/SlingElement.js';
 export { SlingBusinessElement } from './framework/SlingBusinessElement.js';
 export { withLoading } from './v1/src/business/withLoading.js';
+export { withRequestParams } from './v1/src/business/withRequestParams.js';
--- packages/sling-framework/src/v1/src/index.js
+++ packages/sling-framework/src/v1/src/index.js
@@ -2,3 +2,4 @@ export { html } from '../../lib/lit-element.bundle.js';
 export { SlingElement } from './basic/SlingElement.js';
 export { SlingBusinessElement } from './business/SlingBusinessElement.js';
 export { withLoading } from './business/withLoading.js';
+export { withRequestParams } from './business/withRequestParams.js';
