167375bbe08d4cce0b5b1060411ddc098704b44c
Leonardo Favre | Stone <leonardo.favre@stone.com.br>
2018-10-17 18:59:45 -0300

    chore: Expose withRequest and withSetState

--- packages/sling-framework/src/index.js
+++ packages/sling-framework/src/index.js
@@ -2,3 +2,4 @@ export { html } from './lib/lit-element.bundle.js';
 export { SlingElement } from './framework/SlingElement.js';
 export { SlingBusinessElement } from './framework/SlingBusinessElement.js';
 export { withRequest } from './v1/src/business/withRequest.js';
+export { withSetState } from './v1/src/business/withSetState.js';
--- packages/sling-framework/src/v1/src/index.js
+++ packages/sling-framework/src/v1/src/index.js
@@ -1,2 +1,4 @@
 export { html } from '../../lib/lit-element.bundle.js';
 export { SlingElement } from './basic/SlingElement.js';
+export { withRequest } from './business/withRequest.js';
+export { withSetState } from './business/withSetState.js';
