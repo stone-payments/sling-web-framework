907a3c6154628b706b6eb5681f0150b74fba37a8
Victor Mendes <joaovictor_mendes@hotmail.com>
2018-10-17 18:29:41 -0300

    chore: Add text label

--- packages/sling-web-component-label/src/component/Label.js
+++ packages/sling-web-component-label/src/component/Label.js
@@ -3,8 +3,21 @@ import { SlingElement, html } from 'sling-framework';
 export class Label extends SlingElement {
   static get properties() {
     return {
-      
+      type: {
+        type: String,
+        reflectToAttribute: true,
+      },
+      color: {
+        type: String,
+        reflectToAttribute: true,
+      },
     };
+  }
+
+  constructor() {
+    super()
+    this.type = 'fill'
+    this.color = 'green'
   }
 
   render({ disabled, type }) {
@@ -13,17 +26,7 @@ export class Label extends SlingElement {
       <style>
         @import url('sling-web-component-label/src/index.css');
       </style>
-      <span class="common-label red-label">LABEL</span>
-      <span class="common-label yellow-label">LABEL</span>
-      <span class="common-label gray-label">LABEL</span>
-      <span class="common-label green-label">LABEL</span>
-      <span class="common-label purple-label">LABEL</span>
-
-      <span class="common-label red-outline-label">LABEL</span>
-      <span class="common-label yellow-outline-label">LABEL</span>
-      <span class="common-label gray-outline-label">LABEL</span>
-      <span class="common-label green-outline-label">LABEL</span>
-      <span class="common-label purple-outline-label">LABEL</span>
+      <span className="common-label ${this.color}-${this.type}-label"><slot></slot></span>
     `;
   }
 }
