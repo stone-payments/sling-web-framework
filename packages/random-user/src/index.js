5b466d9b48983dcf5d153aae8093f85436d5d2f0
Leonardo Favre | Stone <leonardo.favre@stone.com.br>
2018-10-19 13:14:35 -0300

    chore: Add new example

--- packages/random-user/src/index.js
+++ packages/random-user/src/index.js
@@ -1,39 +1,70 @@
-import { SlingElement, html, withRequest } from 'sling-framework';
+import { SlingElement, html, withRequest, withSetState } from 'sling-framework';
 
-class RandomUser extends withRequest(SlingElement) {
-  static get requestParamNames() {
-    return ['nat'];
+class RandomUser extends withSetState(withRequest(SlingElement)) {
+  constructor() {
+    super();
+    this.state = {
+      name: '',
+      picture: null,
+    };
   }
 
   static get properties() {
     return {
-      user: {
+      state: {
         type: Object,
+        reflectToAttribute: false,
+      },
+      isLoading: {
+        type: Boolean,
+        reflectToAttribute: false,
+      },
+      requestErrors: {
+        type: Array,
         reflectToAttribute: false,
       },
     };
   }
 
-  fetchUser({ nat }) {
-    this.request([
-      fetch(`https://randomuser.me/api/?nat=${nat}`),
-    ]).then((data) => {
-      this.user = data;
-    }).catch((err) => {
-      console.log(err);
-    });
+  static get requestParamNames() {
+    return ['nat'];
   }
 
-  requestParamsChangedCallback({ nat }) {
-    if (nat != null) {
-      this.fetchUser({ nat });
+  requestParamsChangedCallback(requestParams) {
+    if (requestParams.nat != null) {
+      this.fetchUser(requestParams);
     }
+  }
+
+  fetchUser({ nat }) {
+    this
+      .request(window.fetch(`https://randomuser.me/api/?nat=${nat}`))
+      .then(async (response) => {
+        const data = await response.json();
+        const [user] = data.results;
+
+        this.setState({
+          name: `${user.name.first} ${user.name.last}`,
+          picture: user.picture.large,
+        });
+      });
   }
 
   render() {
     return html`
-      ${this.isLoading}
-      ${JSON.stringify(this.user)}
+      <style>
+        img { width: 128px; height: 128px; }
+      </style>
+      ${this.requestErrors.length > 0 ? html`
+        <ul>
+          ${this.requestErrors.map(error => html`<li>${error}</li>`)}
+        </ul>
+      ` : ''}
+      <div>
+        ${this.isLoading ? html`<p><em>loading</em></p>` : ''}
+        ${this.state.picture ? html`<img src="${this.state.picture}">` : ''}
+        <h4>${this.state.name}</h4>
+      </div>
     `;
   }
 }
