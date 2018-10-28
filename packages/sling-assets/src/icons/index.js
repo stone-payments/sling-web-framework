7d063466d8d1087041c50187d30a75a4627265cb
Gabriel Lima <gabrielcastroslima@gmail.com>
2018-10-27 22:27:36 -0300

    Add all other brands

--- packages/sling-assets/src/icons/index.js
+++ packages/sling-assets/src/icons/index.js
@@ -4,6 +4,16 @@ import { CLOSE } from './js/baseline-close-24px.js';
 import { ERROR } from './js/baseline-error-24px.js';
 import { FEEDBACK } from './js/baseline-feedback-24px.js';
 import { VISA } from './js/cardbrand-visa.js';
+import { MASTERCARD } from './js/cardbrand-mastercard.js';
+import { AMERICAN_EXPRESS } from './js/cardbrand-american-express.js';
+import { TICKET } from './js/cardbrand-ticket.js';
+import { SODEXO } from './js/cardbrand-sodexo.js';
+import { VR } from './js/cardbrand-vr.js';
+import { ALELO } from './js/cardbrand-alelo.js';
+import { HIPERCARD } from './js/cardbrand-hipercard.js';
+import { SENFF } from './js/cardbrand-senff';
+import { ELO } from './js/cardbrand-elo';
+import { STONE } from './js/cardbrand-stone';
 
 export {
   CANCEL,
@@ -12,4 +22,14 @@ export {
   ERROR,
   FEEDBACK,
   VISA,
+  MASTERCARD,
+  AMERICAN_EXPRESS,
+  TICKET,
+  SODEXO,
+  VR,
+  ALELO,
+  HIPERCARD,
+  SENFF,
+  ELO,
+  STONE,
 };
