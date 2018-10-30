acfe69953c8956b5a6ed0309c3c68d73aae73d58
Joseli.to <eu@joselitojunior.com>
2018-10-30 12:19:20 -0300

    chore: fix eslint errors

--- packages/sling-web-component-brand-icon/src/component/BrandIcon.test.js
+++ packages/sling-web-component-brand-icon/src/component/BrandIcon.test.js
@@ -16,17 +16,19 @@ describe('Brand Icon', () => {
     $brandIcon = undefined;
   });
 
-  it('Should reflect "brandid", "width" and "heigth" attribute to property ', () => {
+  it('Should reflect "brandid", "width" and "height" '
+  + 'attribute to property ', () => {
     $brandIcon.setAttribute('brandid', '2');
     $brandIcon.setAttribute('width', 50);
     $brandIcon.setAttribute('height', 50);
 
     expect($brandIcon.brandid).to.equal(2);
     expect($brandIcon.width).to.equal('50');
-    expect($brandIcon.height).to.equal('50')
+    expect($brandIcon.height).to.equal('50');
   });
 
-  it('Should reflect "brandid", "width" and "heigth" property to attribute ', () => {
+  it('Should reflect "brandid", "width" and "height" '
+  + ' property to attribute ', () => {
     $brandIcon.brandid = 3;
     $brandIcon.width = 50;
     $brandIcon.height = 50;
@@ -36,7 +38,7 @@ describe('Brand Icon', () => {
     expect($brandIcon.getAttribute('height')).to.equal('50');
   });
 
-  it('Should not set "brandid", "width" and "heigth" property and remove ' +
+  it('Should not set "brandid", "width" and "height" property and remove ' +
     'the respective attribute', () => {
     $brandIcon.brandid = false;
     $brandIcon.width = false;
@@ -44,7 +46,7 @@ describe('Brand Icon', () => {
 
     expect($brandIcon.getAttribute('brandid')).to.equal(null);
     expect($brandIcon.getAttribute('width')).to.equal(null);
-    expect($brandIcon.getAttribute('height')).to.equal(null)
+    expect($brandIcon.getAttribute('height')).to.equal(null);
   });
 
   it('Should get the correct svg based on the id', () => {
@@ -90,6 +92,5 @@ describe('Brand Icon', () => {
       expect(BrandIcon.parseUnit('50px')).to.equal('50px');
       expect(BrandIcon.parseUnit('50%')).to.equal('50%');
     });
-  })
+  });
 });
-
