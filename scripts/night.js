530fb308f1497389860944018aa776f4ab9e6680
Joselito Nascimento | Stone <joselito.nascimento@stone.com.br>
2018-11-05 20:26:46 -0200

    chore: updates node scripts

--- scripts/night.js
+++ scripts/night.js
@@ -1,6 +1,7 @@
 const { exec } = require('shelljs');
 const { join } = require('path');
 const camelcase = require('camelcase');
+const os = require('os');
 
 const getScope = require('./helpers/getScope');
 
@@ -20,6 +21,13 @@ const httpSever = `${httpServerBin} . -s -p 8777 -c-1`;
 
 const testFile = `packages/${scope}/src/component/${camelcase(scope).replace('slingWebComponent', '')}.regression.test.js`;
 
-const killNode = 'kill $(ps aux | grep \'node\' | awk \'{print $2}\')';
 
-exec(`${setupSelenium} && ${httpSever} & ${nightwatchBin} ${testFile} && ${killNode}`);
+if (os.platform() === 'win32') {
+  const killNode = 'taskkill /im node.exe /F';
+  exec(`${setupSelenium}`)
+  exec(`start /min ${httpSever}`);
+  exec(`${nightwatchBin} ${testFile} && ${killNode}`);
+} else {
+  const killNode = 'kill $(ps aux | grep \'node\' | awk \'{print $2}\')';
+  exec(`${setupSelenium} && ${httpSever} & ${nightwatchBin} ${testFile} && ${killNode}`);
+}
