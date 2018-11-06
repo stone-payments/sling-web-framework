96102cc49dc9621136d0132ee9c2232d6020399b
Joselito Nascimento | Stone <joselito.nascimento@stone.com.br>
2018-11-06 15:50:39 -0200

    chore: update test boilerplate

--- packages/sling-web-component-message/src/component/Message.regression.test.js
+++ packages/sling-web-component-message/src/component/Message.regression.test.js
@@ -1,8 +1,10 @@
-module.exports = {
+const component = 'sling-web-component-message';
+const customCode = false;
 
+module.exports = {
   main: (browser) => {
-    browser.url('http://localhost:8080/')
-      .saveScreenshot('./reports/message.png')
+    browser.url(`http://localhost:8777/packages/${component}/${customCode ? 'public/regression' : 'public'}/index.html`)
+      .saveScreenshot(`./reports/${component}/${component}.png`)
       .end();
   },
 };
