4ce2e7c6e001e061f091ec1a30730adf80713710
Renan Barros <renanbarrosrbs@gmail.com>
2018-11-06 19:01:49 -0200

    feat: add login regression test

---
+++ packages/sling-web-component-login/src/component/Login.regression.test.js
@@ -0,0 +1,34 @@
+const component = 'sling-web-component-login';
+
+module.exports = {
+
+  main: (browser) => {
+    const testValues = {
+      email: 'john@doe.com',
+      password: 'password',
+    };
+
+    browser.url('http://localhost:8080/')
+      .saveScreenshot(`./reports/${component}/${component}-start.png`)
+      .execute(function (testValues) { /* eslint-disable-line*/
+        const email = document.querySelector('sling-login').shadowRoot
+          .querySelector('sling-input[id="email"]');
+        email.value = testValues.email;
+        const password = document.querySelector('sling-login').shadowRoot
+          .querySelector('sling-input[id="pw"]');
+        password.value = testValues.password;
+        return { email, password };
+      }, [testValues], function (form) {/* eslint-disable-line*/
+        browser.elementIdAttribute(form.value.email.ELEMENT, 'value',
+          (result) => {
+            browser.assert.equal(result.value, testValues.email);
+          });
+        browser.elementIdAttribute(form.value.password.ELEMENT, 'value',
+          (result) => {
+            browser.assert.equal(result.value, testValues.password);
+          });
+      })
+      .saveScreenshot(`./reports/${component}/${component}-end.png`)
+      .end();
+  },
+};
