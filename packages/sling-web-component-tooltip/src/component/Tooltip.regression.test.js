12d7121aced44b641078a13446584ca12c85ae22
Joselito Nascimento | Stone <joselito.nascimento@stone.com.br>
2018-11-06 15:55:03 -0200

    chore: rewrite tooltip boilerplate

--- packages/sling-web-component-tooltip/src/component/Tooltip.regression.test.js
+++ packages/sling-web-component-tooltip/src/component/Tooltip.regression.test.js
@@ -1,10 +1,12 @@
-module.exports = {
+const component = 'sling-web-component-tooptip';
+const customCode = false;
 
+module.exports = {
   main: (browser) => {
-    browser.url('http://localhost:8080/')
-      .saveScreenshot('./reports/tooltip-no-hover.png')
+    browser.url(`http://localhost:8777/packages/${component}/${customCode ? 'public/regression' : 'public'}/index.html`)
+      .saveScreenshot(`./reports/${component}/${component}-no-hover.png`)
       .moveToElement('sling-tooltip', 0, 0)
-      .saveScreenshot('./reports/tooltip-hover.png')
+      .saveScreenshot(`./reports/${component}/${component}-hover.png`)
       .end();
   },
 };
