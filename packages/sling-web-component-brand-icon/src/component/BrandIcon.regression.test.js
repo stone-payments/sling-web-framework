65a9e3f340745bf76180156b3050af59e0659366
Joselito Nascimento | Stone <joselito.nascimento@stone.com.br>
2018-11-06 15:00:15 -0200

    feat: create a pattern to write regression tests

--- packages/sling-web-component-brand-icon/src/component/BrandIcon.regression.test.js
+++ packages/sling-web-component-brand-icon/src/component/BrandIcon.regression.test.js
@@ -1,8 +1,9 @@
 const component = 'sling-web-component-brand-icon';
+const customCode = false;
 
 module.exports = {
   main: (browser) => {
-    browser.url(`http://localhost:8777/packages/${component}/public/index.html`)
+    browser.url(`http://localhost:8777/packages/${component}/${customCode ? 'public/regression' : 'public'}/index.html`)
       .saveScreenshot(`./reports/${component}/${component}.png`)
       .end();
   },
