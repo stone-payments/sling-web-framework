d747547ced8ce5b4cc3d74a496008cc754440cf4
Joselito Nascimento | Stone <joselito.nascimento@stone.com.br>
2018-11-06 16:45:54 -0200

    chore: update test boilerplate

--- packages/sling-web-component-calendar/src/component/Calendar.regression.test.js
+++ packages/sling-web-component-calendar/src/component/Calendar.regression.test.js
@@ -1,22 +1,23 @@
+const component = 'sling-web-component-calendar';
+const customCode = false;
+
 module.exports = {
-
   main: (browser) => {
-    browser.url('http://localhost:8080/')
-      /* eslint-disable */
-      .execute(function () {
-        return document.querySelector('sling-calendar').shadowRoot.querySelectorAll('.calendar__day')
-      }, [], function (result) {
-        result.value.forEach(calendar => {
+    browser.url(`http://localhost:8777/packages/${component}/${customCode ? 'public/regression' : 'public'}/index.html`)
+      .execute(function () { // eslint-disable-line
+        return document.querySelector('sling-calendar')
+          .shadowRoot.querySelectorAll('.calendar__day');
+      }, [], function (result) { // eslint-disable-line
+        result.value.forEach((calendar) => {
           browser.elementIdClick(calendar.ELEMENT);
-          browser.elementIdAttribute(calendar.ELEMENT, 'class', function (className) {
-            let isSelected = className.value.includes('weekend') || className.value.includes('selected')
-            browser.assert.equal(true, isSelected)
-          })
+          browser.elementIdAttribute(calendar.ELEMENT, 'class', function (className) { // eslint-disable-line
+            const isSelected = className.value.includes('weekend')
+                                 || className.value.includes('selected');
+            browser.assert.equal(true, isSelected);
+          });
         });
-
       });
-    /* eslint-enable */
-    browser.saveScreenshot('./reports/calendar.png')
+    browser.saveScreenshot(`./reports/${component}/${component}.png`)
       .end();
   },
 };
