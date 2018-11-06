125924b5869e98a75c3eaf55033b5d7a7e7264de
Joselito Nascimento | Stone <joselito.nascimento@stone.com.br>
2018-11-06 16:28:48 -0200

    chore: update test boilerplate

--- packages/sling-web-component-select/src/component/Select.regression.test.js
+++ packages/sling-web-component-select/src/component/Select.regression.test.js
@@ -1,20 +1,19 @@
+const component = 'sling-web-component-select';
+const customCode = false;
 
 module.exports = {
-
   main: (browser) => {
-    browser.url('http://localhost:8080/');
-    /* eslint-disable */
-    for (let i = 1; i <= 8; i++) {
-      browser.execute(function (data) {
-        let select = document.querySelector('sling-select')
+    browser.url(`http://localhost:8777/packages/${component}/${customCode ? 'public/regression' : 'public'}/index.html`);
+    for (let i = 1; i <= 8; i++) { // eslint-disable-line
+      browser.execute(function (data) { // eslint-disable-line
+        const select = document.querySelector('sling-select');
         select.value = data;
         return select.shadowRoot.querySelector('select').options[data].text;
-      }, [i], function (result) {
-        browser.assert.equal(result.value, "Option " + i)
+      }, [i], function (result) { // eslint-disable-line
+        browser.assert.equal(result.value, `Option ${i}`);
       });
     }
-    /* eslint-enable */
-    browser.saveScreenshot('./reports/select.png')
+    browser.saveScreenshot(`./reports/${component}/${component}.png`)
       .end();
   },
 };
