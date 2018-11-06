331c893f9199a8efda4e1cdbbb134de3802decb4
Joselito Nascimento | Stone <joselito.nascimento@stone.com.br>
2018-11-06 15:47:11 -0200

    chore: update test boilerplate

--- packages/sling-web-component-loader-wrapper/src/component/LoaderWrapper.regression.test.js
+++ packages/sling-web-component-loader-wrapper/src/component/LoaderWrapper.regression.test.js
@@ -1,8 +1,10 @@
-module.exports = {
+const component = 'sling-web-component-loader-wrapper';
+const customCode = false;
 
+module.exports = {
   main: (browser) => {
-    browser.url('http://localhost:8080/')
-      .saveScreenshot('./reports/loader-wrapper.png')
+    browser.url(`http://localhost:8777/packages/${component}/${customCode ? 'public/regression' : 'public'}/index.html`)
+      .saveScreenshot(`./reports/${component}/${component}.png`)
       .end();
   },
 };
