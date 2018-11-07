0e13e6b588c96bbf487c8da30b5b97f69b571a7c
Renan Barros <renanbarrosrbs@gmail.com>
2018-11-07 19:27:44 -0200

    feat: add label regression test

--- packages/sling-web-component-label/src/component/Label.regression.test.js
+++ packages/sling-web-component-label/src/component/Label.regression.test.js
@@ -1,7 +1,10 @@
+const component = 'sling-web-component-label';
+const customCode = false;
+
 module.exports = {
   main: (browser) => {
-    browser.url('http://localhost:8080/')
-      .saveScreenshot('./reports/label.png')
+    browser.url(`http://localhost:8777/${customCode ? 'regression' : ''}/index.html`)
+      .saveScreenshot(`./reports/${component}/${component}.png`)
       .end();
   },
- };
+};
