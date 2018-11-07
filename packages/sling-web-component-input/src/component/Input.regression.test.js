5b2786f33e4abf917d1d6ffa8adc3725861ba985
Renan Barros <renanbarrosrbs@gmail.com>
2018-11-07 20:16:31 -0200

    feat: add input regression test

--- packages/sling-web-component-input/src/component/Input.regression.test.js
+++ packages/sling-web-component-input/src/component/Input.regression.test.js
@@ -1,49 +1,36 @@
+const component = 'sling-web-component-input';
+const customCode = false;
+
 module.exports = {
-    main: (browser) => {
-        browser.url('http://localhost:8080/')
-        .execute(function (data) {
-
-            var labels = [
-                'sling label',
-                'sling disabled',
-                'sling success',
-                'sling warning',
-                'sling error',
-                '123.123.12312/312',
-                '123.123.123-12',
-                '(12) 1231-2312',
-                '(12) 31231-2312',
-                'abc@def.com',
-                '12312-312'
-            ];
-
-            document.querySelectorAll('sling-input')[0].shadowRoot.querySelector('input').value = labels[0]
-            document.querySelectorAll('sling-input')[1].shadowRoot.querySelector('input').value = labels[1]
-            document.querySelectorAll('sling-input')[2].shadowRoot.querySelector('input').value = labels[2]
-            document.querySelectorAll('sling-input')[3].shadowRoot.querySelector('input').value = labels[3]
-            document.querySelectorAll('sling-input')[4].shadowRoot.querySelector('input').value = labels[4]
-            document.querySelectorAll('sling-input')[5].shadowRoot.querySelector('input').value = labels[5]
-            document.querySelectorAll('sling-input')[6].shadowRoot.querySelector('input').value = labels[6]
-            document.querySelectorAll('sling-input')[7].shadowRoot.querySelector('input').value = labels[7]
-            document.querySelectorAll('sling-input')[8].shadowRoot.querySelector('input').value = labels[8]
-            document.querySelectorAll('sling-input')[9].shadowRoot.querySelector('input').value = labels[9]
-            document.querySelectorAll('sling-input')[10].shadowRoot.querySelector('input').value = labels[10]
-
-            return labels
-        }, [], function(result) {
-            browser.assert.equal(result.value[0], 'sling label')
-            browser.assert.equal(result.value[1], 'sling disabled')
-            browser.assert.equal(result.value[2], 'sling success')
-            browser.assert.equal(result.value[3], 'sling warning')
-            browser.assert.equal(result.value[4], 'sling error')
-            browser.assert.equal(result.value[5], '123.123.12312/312')
-            browser.assert.equal(result.value[6], '123.123.123-12')
-            browser.assert.equal(result.value[7], '(12) 1231-2312')
-            browser.assert.equal(result.value[8], '(12) 31231-2312')
-            browser.assert.equal(result.value[9], 'abc@def.com')
-            browser.assert.equal(result.value[10], '12312-312')
-        })
-        browser.saveScreenshot('./reports/input.png')
-        .end();
-    },
+  main: (browser) => {
+    const testValues = [
+      'sling label',
+      'sling disabled',
+      'sling success',
+      'sling warning',
+      'sling error',
+      '12.312.312/3123-12',
+      '123.123.123-12',
+      '(12) 1231-2312',
+      '(12) 31231-2312',
+      'john@doe.com',
+      '12312-312',
+    ];
+    browser.url(`http://localhost:8777/${customCode ? 'regression' : ''}/index.html`)
+      .execute(function (testValues) { //eslint-disable-line
+        const inputList = document.querySelectorAll('sling-input');
+        inputList.forEach((input, i) => {
+          input.value = testValues[i];
+        });
+        return inputList;
+      }, [testValues], function (inputList) {//eslint-disable-line
+        inputList.value.forEach((input, i) => {
+          browser.elementIdAttribute(input.ELEMENT, 'value', (inputValue) => {
+            browser.assert.equal(inputValue.value, testValues[i]);
+          });
+        });
+      })
+      .saveScreenshot(`./reports/${component}/${component}.png`)
+      .end();
+  },
 };
