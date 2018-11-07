8a2f756477e2833a3e52d4e1128f8cf2731fd9e6
Renan Barros <renanbarrosrbs@gmail.com>
2018-11-07 17:52:30 -0200

    feat: add calendar regression test

---
+++ packages/sling-web-component-calendar/src/component/Calendar.regression.test.js
@@ -0,0 +1,23 @@
+const component = 'sling-web-component-calendar';
+const customCode = false;
+
+module.exports = {
+  main: (browser) => {
+    browser.url(`http://localhost:8777/${customCode ? 'regression' : ''}/index.html`)
+      .execute(function () { // eslint-disable-line
+        return document.querySelector('sling-calendar')
+          .shadowRoot.querySelectorAll('.calendar__day');
+      }, [], function (result) { // eslint-disable-line
+        result.value.forEach((calendar) => {
+          browser.elementIdClick(calendar.ELEMENT);
+          browser.elementIdAttribute(calendar.ELEMENT, 'class', function (className) { // eslint-disable-line
+            const isSelected = className.value.includes('weekend')
+                                 || className.value.includes('selected');
+            browser.assert.equal(true, isSelected);
+          });
+        });
+      });
+    browser.saveScreenshot(`./reports/${component}/${component}.png`)
+      .end();
+  },
+};
