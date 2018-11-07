3eb935d16e5b70674a72a56b2f9688bf7bc3849c
Renan Barros <renanbarrosrbs@gmail.com>
2018-11-07 18:11:19 -0200

    chore: update snackbar test url

--- packages/sling-web-component-snackbar/src/component/SnackBar.regression.test.js
+++ packages/sling-web-component-snackbar/src/component/SnackBar.regression.test.js
@@ -1,16 +1,19 @@
+const component = 'sling-web-component-snackbar';
+const customCode = false;
+
 module.exports = {
 
   main: (browser) => {
-    browser.url('http://localhost:8080/')
-      .saveScreenshot('./reports/snackbar-start.png')
+    browser.url(`http://localhost:8777/${customCode ? 'regression' : ''}/index.html`)
+      .saveScreenshot(`./reports/${component}/${component}-start.png`)
       .elements('css selector', 'sling-button', (buttons) => {
         buttons.value.forEach((button) => {
           browser.elementIdClick(button.ELEMENT);
         });
       })
-      .saveScreenshot('./reports/snackbar-mid.png')
+      .saveScreenshot(`./reports/${component}/${component}-mid.png`)
       .execute('window.scrollTo(0,document.body.scrollHeight);')
-      .saveScreenshot('./reports/snackbar-end.png')
+      .saveScreenshot(`./reports/${component}/${component}-end.png`)
       .end();
   },
 };
