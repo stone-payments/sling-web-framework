7a5f5ce02094679c4c83d3d9685b468db65e6716
Renan Barros <renanbarrosrbs@gmail.com>
2018-11-07 17:11:29 -0200

    chore: add nightwatch urls

--- packages/sling-web-component-tooltip/src/component/Tooltip.regression.test.js
+++ packages/sling-web-component-tooltip/src/component/Tooltip.regression.test.js
@@ -3,7 +3,7 @@ const customCode = false;
 
 module.exports = {
   main: (browser) => {
-    browser.url(`http://localhost:8777/packages/${component}/${customCode ? 'public/regression' : 'public'}/index.html`)
+    browser.url(`http://localhost:8777/${customCode ? 'regression' : ''}/index.html`)
       .saveScreenshot(`./reports/${component}/${component}-no-hover.png`)
       .moveToElement('sling-tooltip', 0, 0)
       .saveScreenshot(`./reports/${component}/${component}-hover.png`)
