b2a8cf3f4bf22bf5b59e38a66603f00e0203c40c
Leonardo Favre | Stone <leonardo.favre@stone.com.br>
2018-12-03 16:11:42 -0200

    Implement setFieldValue and touchField

--- packages/sling-web-component-form/src/state/FormReducer.js
+++ packages/sling-web-component-form/src/state/FormReducer.js
@@ -12,16 +12,36 @@ const INITIAL_STATE = {
 };
 
 export const SET_DIRTY = 'SET_DIRTY';
+export const SET_FIELD_VALUE = 'SET_FIELD_VALUE';
+export const TOUCH_FIELD = 'TOUCH_FIELD';
 
 export const setDirty = dirty => ({
   type: SET_DIRTY,
   dirty,
 });
 
+export const setFieldValue = (fieldId, value) => ({
+  type: SET_FIELD_VALUE,
+  fieldId,
+  value,
+});
+
+export const touchField = fieldId => ({
+  type: TOUCH_FIELD,
+  fieldId,
+});
+
 export const FormReducer = (state = INITIAL_STATE, action = {}) => {
   switch (action.type) {
     case SET_DIRTY:
       return setIn(state, 'dirty', action.dirty);
+
+    case SET_FIELD_VALUE:
+      return setIn(state, `values.${action.fieldId}`,
+        (typeof action.value === 'undefined') ? null : action.value);
+
+    case TOUCH_FIELD:
+      return setIn(state, `touched.${action.fieldId}`, true);
 
     default:
       return state;
