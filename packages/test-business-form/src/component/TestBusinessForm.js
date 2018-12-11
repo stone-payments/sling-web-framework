1c48375cd58819cde8ec3b4afd48133ff3ce7622
leofavre <contato@leofavre.com>
2018-12-11 11:25:28 -0200

    Remove LitElement bindings to form

--- packages/test-business-form/src/component/TestBusinessForm.js
+++ packages/test-business-form/src/component/TestBusinessForm.js
@@ -2,100 +2,12 @@ import { TestBusinessFormView } from './TestBusinessFormView.js';
 import * as validations from './validations.js';
 
 export const TestBusinessForm = Base => class extends Base {
-  constructor() {
-    super();
-    this.addFriend = this.addFriend.bind(this);
-    this.removeFriend = this.removeFriend.bind(this);
-    this.handleFormUpdate = this.handleFormUpdate.bind(this);
-    this.handleFormSubmitSucess = this.handleFormSubmitSucess.bind(this);
-  }
-
   connectedCallback() {
     super.connectedCallback();
     this.formElement = this.shadowRoot.querySelector('sling-form');
   }
 
-  static get properties() {
-    return {
-      form: {
-        type: Object,
-        reflectToAttribute: false,
-      },
-    };
-  }
-
-  handleFormUpdate(evt) {
-    this.form = evt.detail;
-  }
-
-  handleFormSubmitSucess(evt) {
-    console.log(this, evt.detail);
-  }
-
-  addFriend() {
-    this.formElement.values = {
-      ...this.formElement.values,
-      friends: [
-        ...this.formElement.values.friends || [],
-        { name: '' },
-      ],
-    };
-
-    this.formElement.errors = {
-      ...this.formElement.errors,
-      friends: [
-        ...this.formElement.errors.friends || [],
-        { name: null },
-      ],
-    };
-
-    this.formElement.touched = {
-      ...this.formElement.touched,
-      friends: [
-        ...this.formElement.touched.friends || [],
-        { name: false },
-      ],
-    };
-  }
-
-  removeFriend(index) {
-    return () => {
-      this.formElement.values = {
-        ...this.formElement.values,
-        friends: this.formElement.values
-          .friends.filter((_, idx) => idx !== index),
-      };
-
-      this.formElement.errors = {
-        ...this.formElement.errors,
-        friends: this.formElement.errors
-          .friends.filter((_, idx) => idx !== index),
-      };
-
-      this.formElement.touched = {
-        ...this.formElement.touched,
-        friends: this.formElement.touched
-          .friends.filter((_, idx) => idx !== index),
-      };
-    };
-  }
-
   render() {
-    const {
-      form,
-      handleFormUpdate,
-      handleFormSubmitSucess,
-      addFriend,
-      removeFriend,
-    } = this;
-
-    return TestBusinessFormView({
-      ...form,
-      ...validations,
-      handleFormUpdate,
-      handleFormSubmitSucess,
-      addFriend,
-      removeFriend,
-    });
+    return TestBusinessFormView({ ...validations });
   }
 };
--- packages/test-business-form/src/component/TestBusinessFormView.js
+++ packages/test-business-form/src/component/TestBusinessFormView.js
@@ -4,115 +4,49 @@ import 'sling-web-component-input';
 import 'sling-web-component-button';
 
 export const TestBusinessFormView = ({
-  values = {},
-  errors = {},
-  isValid,
-  isValidating,
-  isSubmitting,
-  validateRequiredField,
+  validateTakenUsername,
   validateRequiredEmail,
   validateOptionalPhone,
   validateForm,
-  validateTakenUsername,
-  handleFormUpdate,
-  handleFormSubmitSucess,
-  addFriend,
-  removeFriend,
 }) => html`
   <sling-form
-    validation="${validateForm}"
-    onupdate="${handleFormUpdate}"
-    onsubmitsuccess="${handleFormSubmitSucess}">
+    validation="${validateForm}">
     <label>
       <h4>Apelido</h4>
       <sling-input
         name="username"
         type="text"
-        value="${values.username}"
         validation="${validateTakenUsername}"></sling-input>
     </label>
-
-    ${errors.username ? html`
-      <p>${errors.username}</p>
-    ` : ''}
 
     <label>
       <h4>E-mail</h4>
       <sling-input
         name="email"
         type="email"
-        value="${values.email}"
         validation="${validateRequiredEmail}"></sling-input>
     </label>
 
-    ${errors.email ? html`
-      <p>${errors.email}</p>
-    ` : ''}
-
     <h4>Telefone(s)</h4>
-
-    ${errors.onePhoneMinimum ? html`
-      <p>${errors.onePhoneMinimum}</p>
-    ` : ''}
 
     <label>
       <span>Pessoal</span>
       <sling-input
         name="phone.personal"
         type="phone"
-        value="${values.phone && values.phone.personal}"
         validation="${validateOptionalPhone}"></sling-input>
     </label>
-
-    ${errors.phone && errors.phone.personal ? html`
-      <p>${errors.phone.personal}</p>
-    ` : ''}
 
     <label>
       <span>Trabalho</span>
       <sling-input
         name="phone.work"
         type="phone"
-        value="${values.phone && values.phone.work}"
         validation="${validateOptionalPhone}"></sling-input>
     </label>
 
-    ${errors.phone && errors.phone.work ? html`
-      <p>${errors.phone.work}</p>
-    ` : ''}
-  
-    <h4>
-      Amigos
-      <sling-button
-        type="button"
-        onclick="${addFriend}">
-        Adicionar
-      </sling-button>
-    </h4>
-
-    ${values.friends && values.friends.map((friend, index) => html`
-      <label style="display: grid; grid-template-columns: 4fr 1fr">
-        <span style="grid-column: 1 / span 2">Nome do amigo</span>
-        <sling-input
-          name="friends[${index}].name"
-          type="text"
-          value="${friend && friend.name}"
-          validation="${validateRequiredField}"></sling-input>
-        <sling-button
-          type="button"
-          onclick="${removeFriend(index)}">
-          Remover amigo
-        </sling-button>
-      </label>
-
-      ${errors.friends && errors.friends[index] && errors.friends[index].name ? html`
-        <p>${errors.friends[index].name}</p>
-      ` : ''}
-    `)}
-
     <sling-button
-      type="submit"
-      disabled="${isValidating || !isValid || isSubmitting}">
+      type="submit">
       Enviar
     </sling-button>
   </sling-form>
