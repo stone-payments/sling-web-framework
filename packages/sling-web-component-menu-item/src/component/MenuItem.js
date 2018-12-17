ed610141af7ac2e09a62ec203c13fa57c3f8c73b
Joselito <eu@joseli.to>
2018-12-17 16:30:52 -0200

    chore: tweak some css

--- packages/sling-web-component-menu-item/src/component/MenuItem.js
+++ packages/sling-web-component-menu-item/src/component/MenuItem.js
@@ -21,27 +21,46 @@ export class MenuItem extends SlingElement {
   }
 
   render() {
-    const menuSize = this.parentElement.getAttribute('size');
+    const menuSize = this.parentElement.getAttribute('size') || '';
+    const layoutType = this.parentElement.getAttribute('layout') || '';
+    const anchorHref = this.getAttribute('href');
+    
+    const isActive = (this.getAttribute('active') != null) ? 'active' : '';
+
     const childrens = Array.from(this.children).map($el => $el.nodeName);
     const hasMenu = childrens.includes('SLING-MENU');
 
-    return !hasMenu ? html`
+    return (anchorHref && !hasMenu) ? html`
       <style>
         @import url('sling-web-component-menu-item/src/index.css');
       </style>
-      <a href="http://google.com">
-        <sling-icon icon="${this.aim || 'info'}" className="${menuSize}"></sling-icon>
-        <slot></slot>
-      </a>
+        ${this.aim ? html`
+          <a href="${anchorHref}" className="${layoutType} ${menuSize} ${isActive} hasIcon">
+            <sling-icon icon="${this.aim}" className="${menuSize}"></sling-icon>
+            <slot></slot>
+          </a>
+        ` : html`
+          <a href="${anchorHref}" className="${layoutType} ${menuSize} ${isActive}">
+            <slot></slot>
+          </a>
+        `}
     ` : html`
       <style>
         @import url('sling-web-component-menu-item/src/index.css');
       </style>
-      <a href="http://google.com">
-        <sling-icon icon="${this.aim || 'info'}" className="${menuSize}"></sling-icon>
-        <slot name="title"></slot>
-      </a>
-      <slot></slot>
+        ${this.aim ? html`
+          <a className="${layoutType} ${menuSize} ${isActive} hasIcon">
+            <sling-icon icon="${this.aim || 'info'}" className="${menuSize}"></sling-icon>
+            <slot name="title"></slot>
+          </a>
+          <slot></slot>
+        ` : html`
+          <a className="${layoutType} ${menuSize} ${isActive}">
+            <slot name="title"></slot>
+          </a>
+          <slot></slot>
+        `}
+      
     `;
   }
 }
