2ee42211869e8091c28a1027696d55a32c07887e
Leonardo Favre | Stone <leonardo.favre@stone.com.br>
2018-12-18 18:26:33 -0200

    Remove original POC

--- packages/test-business-form/src/index.js
+++
@@ -1,5 +0,0 @@
-import { SlingElement } from 'sling-framework';
-import { registerComponent } from 'sling-helpers';
-import { TestBusinessForm } from './component/TestBusinessForm.js';
-
-registerComponent('test-business-form', TestBusinessForm(SlingElement));
