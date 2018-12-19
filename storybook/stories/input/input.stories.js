c644eff13d56574e36c2cb84d034763b01064e29
Unknown <joaovictor_mendes@hotmail.com>
2018-12-19 13:56:22 -0200

    fix: eslint

--- storybook/stories/input/input.stories.js
+++ storybook/stories/input/input.stories.js
@@ -1,13 +1,11 @@
 import { storiesOf } from '@storybook/vue';
-import { action } from '@storybook/addon-actions';
-import { linkTo } from '@storybook/addon-links';
 
 import StorybookInput from './Input';
 
 const stories = storiesOf('Input', module);
 
 stories
-  	.add('List of input types', () => ({
-    	components: { StorybookInput },
-    	template: '<storybook-Input />'
-  	}));
+	.add('List of input types', () => ({
+  	components: { StorybookInput },
+  	template: '<storybook-Input />',
+	}));
