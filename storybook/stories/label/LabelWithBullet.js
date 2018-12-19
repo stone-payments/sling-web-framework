191040ba5df71f68c2385ab1fb8d5888de44df47
Unknown <joaovictor_mendes@hotmail.com>
2018-12-19 14:49:47 -0200

    feat: Add label storyboard

--- storybook/stories/label/LabelWithBullet.js
+++ storybook/stories/label/LabelWithBullet.js
@@ -6,7 +6,25 @@ export default {
     template: `
         <div>
             <h1>Small</h1>
-            
+            <sling-label size="small" color="red" type="text" showBullet="true">LABEL</sling-label>
+            <sling-label size="small" color="yellow" type="text" showBullet="true">LABEL</sling-label>
+            <sling-label size="small" color="gray" type="text" showBullet="true">LABEL</sling-label>
+            <sling-label size="small" color="green" type="text" showBullet="true">LABEL</sling-label>
+            <sling-label size="small" color="purple" type="text" showBullet="true">LABEL</sling-label>
+
+            <h1>Normal</h1>
+            <sling-label size="medium" color="red" type="text" showBullet="true">LABEL</sling-label>
+            <sling-label size="medium" color="yellow" type="text" showBullet="true">LABEL</sling-label>
+            <sling-label size="medium" color="gray" type="text" showBullet="true">LABEL</sling-label>
+            <sling-label size="medium" color="green" type="text" showBullet="true">LABEL</sling-label>
+            <sling-label size="medium" color="purple" type="text" showBullet="true">LABEL</sling-label>
+
+            <h1>Big</h1>
+            <sling-label size="big" color="red" type="text" showBullet="true">LABEL</sling-label>
+            <sling-label size="big" color="yellow" type="text" showBullet="true">LABEL</sling-label>
+            <sling-label size="big" color="gray" type="text" showBullet="true">LABEL</sling-label>
+            <sling-label size="big" color="green" type="text" showBullet="true">LABEL</sling-label>
+            <sling-label size="big" color="purple" type="text" showBullet="true">LABEL</sling-label>
         </div>
     `,
 
--- storybook/stories/label/label.stories.js
+++ storybook/stories/label/label.stories.js
@@ -1,6 +1,4 @@
 import { storiesOf } from '@storybook/vue';
-import { action } from '@storybook/addon-actions';
-import { linkTo } from '@storybook/addon-links';
 
 import StorybookFillLabel from './LabelFill';
 import StorybookOutlineLabel from './LabelOutline';
@@ -11,23 +9,23 @@ import StorybookWithIconLabel from './LabelWithIcon';
 const stories = storiesOf('Label', module);
 
 stories
-  	.add('Fill', () => ({
-    	components: { StorybookFillLabel },
-    	template: '<storybook-fill-label />'
-  	}))
-  	.add('Outline', () => ({
-    	components: { StorybookOutlineLabel },
-    	template: '<storybook-outline-label />'
-  	}))
-  	.add('Only text', () => ({
-    	components: { StorybookOnlyTextLabel },
-    	template: '<storybook-only-text-label />'
-  	}))
-  	.add('With bullet', () => ({
-    	components: { StorybookWithBulletLabel },
-    	template: '<storybook-with-bullet-label />'
-  	}))
-  	.add('With Icon', () => ({
-    	components: { StorybookWithIconLabel },
-    	template: '<storybook-with-icon-label />'
-  	}));
+  .add('Fill', () => ({
+    components: { StorybookFillLabel },
+    template: '<storybook-fill-label />',
+  }))
+  .add('Outline', () => ({
+    components: { StorybookOutlineLabel },
+    template: '<storybook-outline-label />',
+  }))
+  .add('Only text', () => ({
+    components: { StorybookOnlyTextLabel },
+    template: '<storybook-only-text-label />',
+  }))
+  .add('With bullet', () => ({
+    components: { StorybookWithBulletLabel },
+    template: '<storybook-with-bullet-label />',
+  }))
+  .add('With Icon', () => ({
+    components: { StorybookWithIconLabel },
+    template: '<storybook-with-icon-label />',
+  }));
