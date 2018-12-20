4e08b378532d814cc71c66d2847a0dc10f2ee712
Unknown <joaovictor_mendes@hotmail.com>
2018-12-20 11:41:37 -0200

    chore: Add values to select

--- storybook/stories/select/Select.js
+++ storybook/stories/select/Select.js
@@ -13,4 +13,45 @@ export default {
         </div>
     `,
 
+    mounted: () => {
+        const mockData = [
+		    {
+		      name: 'Option 1',
+		      id: 1,
+		    },
+		    {
+		      name: 'Option 2',
+		      id: 2,
+		    },
+		    {
+		      name: 'Option 3',
+		      id: 3,
+		    },
+		    {
+		      name: 'Option 4',
+		      id: 4,
+		    },
+		    {
+		      name: 'Option 5',
+		      id: 5,
+		    },
+		    {
+		      name: 'Option 6',
+		      id: 6,
+		    },
+		    {
+		      name: 'Option 7',
+		      id: 7,
+		    },
+		    {
+		      name: 'Option 8',
+		      id: 8,
+		    },
+	  	];
+
+	  const $select = document.querySelector('sling-select');
+
+	  $select.srcoptions = mockData;
+    }
+
 };
