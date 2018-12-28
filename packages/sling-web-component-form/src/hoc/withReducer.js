a80b29c0ae7fa564ae77cf59ef02be4b3b965c4e
leofavre <contato@leofavre.com>
2018-12-28 19:20:13 -0200

    Add tests to withReducer

--- packages/sling-web-component-form/src/hoc/withReducer.js
+++ packages/sling-web-component-form/src/hoc/withReducer.js
@@ -1,26 +1,26 @@
 import { isFunction, isPromise } from 'sling-helpers';
 
-export const withReducer = reducer => Base => class extends Base {
-  constructor() {
-    super();
-    this.reducer = reducer;
-    this.state = this.reducer();
-  }
-
-  dispatchAction(action) {
-    let resolvedAction = action;
-    const getState = () => this.state;
-
-    if (isFunction(resolvedAction)) {
-      resolvedAction = action(this.dispatchAction.bind(this), getState);
+export const withReducer = reducer => (Base = class {}) =>
+  class extends (Base) {
+    constructor() {
+      super();
+      this.state = reducer();
     }
 
-    if (isPromise(resolvedAction)) {
-      resolvedAction.then((asyncAction) => {
-        this.state = this.reducer(this.state, asyncAction);
-      });
-    }
+    dispatchAction(action) {
+      let resolvedAction = action;
+      const getState = () => this.state;
 
-    this.state = this.reducer(this.state, resolvedAction);
-  }
-};
+      if (isFunction(resolvedAction)) {
+        resolvedAction = action(this.dispatchAction.bind(this), getState);
+      }
+
+      if (isPromise(resolvedAction)) {
+        resolvedAction.then((asyncAction) => {
+          this.state = reducer(this.state, asyncAction);
+        });
+      }
+
+      this.state = reducer(this.state, resolvedAction);
+    }
+  };
