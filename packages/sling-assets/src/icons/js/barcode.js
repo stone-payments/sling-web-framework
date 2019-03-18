cac3ac3d5ad36116d2c29c5e5f0a4aeeb29c9232
Andreolle Dantas <andreolle18@gmail.com>
2019-03-18 16:55:19 -0300

    adding barcode.svg

---
+++ packages/sling-assets/src/icons/js/barcode.js
@@ -0,0 +1,5 @@
+export const BARCODE =
+  `<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M10 21L10 3L8 3L8 21L10 21ZM12 21L12 3L11 3L11 21L12 21ZM17 21L17 3L14 3L14 21L17 21ZM4 21L7 21L7 3L4 3L4 21Z" fill="black"/>
+    <path d="M20 3L20 21L18 21L18 3L20 3Z" fill="black"/>
+  </svg>`;
