3aa78d18d3403b6428ea0594a29258f1a37e9b19
caiomdias <caiomdias_@hotmail.com>
2019-07-02 01:37:02 -0300

    remove emd asstes sym and add emd-assets dependency

--- packages/emd-assets/src/icons/views/arrowLeftAlt.js
+++
@@ -1,7 +0,0 @@
-export const ARROW_LEFT_ALT = renderer => renderer`
-<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-  <g>
-    <path fill-rule="evenodd" clip-rule="evenodd" d="M12 20L13.41 18.59L7.82999 13L20 13L20 11L7.82999 11L13.41 5.41L12 4L3.99999 12L12 20Z" />
-  </g>
-</svg>
-`;
--- packages/emd-assets/src/icons/views/arrowUpAlt.js
+++
@@ -1,7 +0,0 @@
-export const ARROW_UP_ALT = renderer => renderer`
-<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-  <g>
-    <path fill-rule="evenodd" clip-rule="evenodd" d="M4 12L5.41 13.41L11 7.83L11 20L13 20L13 7.83L18.59 13.41L20 12L12 4L4 12Z" />
-  </g>
-</svg>
-`;
