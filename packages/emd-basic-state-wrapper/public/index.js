dd5a9bf7605f2c51514f84caec6e25915ac2eb76
Leonardo Favre | Stone <leonardo.favre@stone.com.br>
2019-08-19 19:53:39 -0300

    chore: Change test not to us a business component

--- packages/emd-basic-state-wrapper/public/index.js
+++ packages/emd-basic-state-wrapper/public/index.js
@@ -1,13 +1,30 @@
-import { v0 as sdk } from 'customer-js-sdk';
+import { rows } from './rows.js';
+import '@stone-payments/emd-basic-table';
 import '../src/index.js';
-import '@stone-payments/emd-business-weekly-calendar';
-
-sdk.authorization.set(sdk.BEARER, ``);
 
 let currentButton = 'default';
 
 const wrappers = Array.from(document.querySelectorAll('emd-state-wrapper'));
-const businesses = Array.from(document.querySelectorAll('emd-weekly-calendar'));
+const tables = Array.from(document.querySelectorAll('emd-table'));
+
+const header = ['Bandeira', 'Valor Pago'];
+
+const appearance = {
+  align: ['left', 'right'],
+  valign: 'middle'
+};
+
+const adapter = ({ value, currency, brand, date }) => [
+  brand,
+  value
+];
+
+tables.forEach(table => {
+  table.rows = rows;
+  table.adapter = adapter;
+  table.appearance = appearance;
+  table.header = header;
+});
 
 const stateButtons = Array
   .from(document.querySelectorAll('button'))
@@ -41,56 +58,10 @@ stateButtons.forEach(button => {
       wrapper.currentState = currentButton;
     });
   };
+
+  wrappers.forEach(wrapper => {
+    wrapper.currentState = 'default';
+  });
 });
 
 updateStateButtons();
-
-businesses.forEach(business => {
-  business.source = [
-    {
-      date: '2019-01-31',
-      value: 48.96,
-      currency: 'BRL'
-    },
-    {
-      date: '2019-02-01',
-      value: 73.3,
-      currency: 'BRL'
-    },
-    {
-      date: '2019-02-04',
-      value: 0,
-      currency: 'BRL'
-    },
-    {
-      date: '2019-02-05',
-      value: 0,
-      currency: 'BRL'
-    },
-    {
-      date: '2019-02-06',
-      value: 265.01,
-      currency: 'BRL'
-    },
-    {
-      date: '2019-02-01',
-      value: 73.3,
-      currency: 'BRL'
-    },
-    {
-      date: '2019-02-04',
-      value: 0,
-      currency: 'BRL'
-    },
-    {
-      date: '2019-02-05',
-      value: 0,
-      currency: 'BRL'
-    },
-    {
-      date: '2019-02-06',
-      value: 265.01,
-      currency: 'BRL'
-    }
-  ];
-});
---
+++ packages/emd-basic-state-wrapper/public/rows.js
@@ -0,0 +1,44 @@
+export const rows = [
+  {
+    date: '2019-03-30T12:25:57.6672005+00:00',
+    value: 7.5,
+    currency: 'BRL',
+    brand: 'elo'
+  },
+  {
+    date: '2019-03-30T12:25:57.5755048+00:00',
+    value: 420,
+    currency: 'BRL',
+    brand: 'mastercard'
+  },
+  {
+    date: '2019-03-30T12:25:57.3247419+00:00',
+    value: 40,
+    currency: 'BRL',
+    brand: 'mastercard'
+  },
+  {
+    date: '2019-03-30T12:25:57.2519149+00:00',
+    value: 803.25,
+    currency: 'BRL',
+    brand: 'mastercard'
+  },
+  {
+    date: '2019-03-30T12:25:56.5328065+00:00',
+    value: 82.8,
+    currency: 'BRL',
+    brand: 'mastercard'
+  },
+  {
+    date: '2019-03-30T12:25:57.5755048+00:00',
+    value: 420,
+    currency: 'BRL',
+    brand: 'mastercard'
+  },
+  {
+    date: '2019-03-30T12:25:57.6672005+00:00',
+    value: 7.5,
+    currency: 'BRL',
+    brand: 'elo'
+  }
+];
