94befd926a39b4f86584332535637d332c61a4d8
Juliana Schneider <juliana.schneider.ime@gmail.com>
2019-09-09 18:28:18 -0300

    chore: Fix strings to use single quotes

--- packages/emd-basic-brand-icon/src/component/BrandIconController.js
+++ packages/emd-basic-brand-icon/src/component/BrandIconController.js
@@ -1,7 +1,7 @@
 export const BrandIconController = (Base = class {}) => class extends Base {
   static getIconByName (name) {
-    const key = name.normalize("NFD")
-      .replace(/[\u0300-\u036f]/g, "")
+    const key = name.normalize('NFD')
+      .replace(/[\u0300-\u036f]/g, '')
       .replace(/-/g, '_')
       .replace(' ', '_')
       .toUpperCase();
