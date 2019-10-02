781bbbfd1ee2a0f90c455302ff2a457856f2852e
Leonardo Favre <leo@leofavre.com>
2019-10-02 11:41:06 -0300

    feat: Added Loading Wrapper component

--- packages/emd-basic-loading-wrapper/src/component/LoadingWrapperView.js
+++ packages/emd-basic-loading-wrapper/src/component/LoadingWrapperView.js
@@ -1,12 +1,27 @@
 import { html } from '@stone-payments/lit-element';
 import '@stone-payments/emd-basic-loader';
 
-export const LoadingWrapperView = () => html`
-  <style>
-    @import url("emd-basic-loading-wrapper/src/component/LoadingWrapper.css")
-  </style>
-  <div class="emd-loading-wrapper__wrapper">
-    <span><emd-loader loading></emd-loader></span>
-    <slot></slot>
-  </div>
-`;
+export const LoadingWrapperView = ({
+  loading,
+  handleLoadingStart,
+  handleLoadingEnd
+}) => {
+  let wrapperClass = 'loading-wrapper';
+  wrapperClass += (loading ? ' loading-wrapper_loading' : '');
+
+  return html`
+    <style>
+      @import url("emd-basic-loading-wrapper/src/component/LoadingWrapper.css")
+    </style>
+    <div
+      class="${wrapperClass}"
+      @loadingstart="${handleLoadingStart}"
+      @loadingend="${handleLoadingEnd}"
+    >
+      <div class="loading-wrapper__loader">
+        <emd-loader loading></emd-loader>
+      </div>
+      <div class="loading-wrapper__slot"><slot></slot></div>
+    </div>
+  `;
+};
