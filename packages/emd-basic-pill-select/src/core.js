46af4ec92b44aa436b9bf57484c5416375a72d4a
Leonardo Favre <leo@leofavre.com>
2019-12-03 17:41:44 -0300

    chore: use withField

--- packages/emd-basic-pill-select/src/core.js
+++ packages/emd-basic-pill-select/src/core.js
@@ -1,5 +1,5 @@
 import { compose } from '@stone-payments/emd-helpers';
-import { withComponent } from '@stone-payments/emd-hocs';
+import { withComponent, withField } from '@stone-payments/emd-hocs';
 import { LitElement } from '@stone-payments/lit-element';
 
 import { PillSelectController } from './component/PillSelectController.js';
@@ -7,6 +7,7 @@ import { PillSelectView } from './component/PillSelectView.js';
 
 const PillSelect = compose(
   PillSelectController,
+  withField,
   withComponent
 )(LitElement);
 
