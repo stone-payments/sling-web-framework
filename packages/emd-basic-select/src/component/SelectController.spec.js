48659f53fc31c9b8faedb315eec704d58ed17756
Leonardo Favre <leo@leofavre.com>
2019-12-04 14:04:10 -0300

    wip: Add tests to SelectController

--- packages/emd-basic-select/src/component/SelectController.spec.js
+++ packages/emd-basic-select/src/component/SelectController.spec.js
@@ -172,10 +172,10 @@ describe('SelectController', () => {
         .to.have.been.calledWith('Summer');
     });
 
-    it('Should update the view', async () => {
-      element.options = [];
-      await Promise.resolve();
-      expect(element._updateView).to.have.been.called;
+    it('Should update the view', () => {
+      element._isValidValue = () => true;
+      element.value = 'Summer';
+      expect(element._updateView).to.have.been.calledOnce;
     });
 
     it('Should only set the initial value if the DOM field ' +
@@ -190,20 +190,94 @@ describe('SelectController', () => {
   });
 
   describe('#options', () => {
-    it('Should', () => {
+    beforeEach(() => {
+      const FAKE_DOM_SELECT = { value: '' };
 
+      element.renderRoot = {
+        querySelector: sinon.stub().withArgs('select').returns(FAKE_DOM_SELECT)
+      };
+
+      Object.defineProperty(element, '_processedFieldValue', {
+        get () {
+          return element.field
+            ? String(element.field.value)
+            : '';
+        }
+      });
+
+      element._updateView = sinon.spy();
     });
-  });
 
-  describe('#parsedOptions', () => {
-    it('Should', () => {
+    it('Should keep a valid value when setting options', async () => {
+      element.options = ['Spring', 'Summer', 'Autumn', 'Winter'];
+      element.value = 'Summer';
+      await Promise.resolve();
+      expect(element.value).to.equal('Summer');
+    });
 
+    it('Should discard a previously valid value when setting ' +
+      'options', async () => {
+      element.options = ['Spring', 'Summer', 'Autumn', 'Winter'];
+      element.value = 'Fall';
+      await Promise.resolve();
+      expect(element.value).to.equal('');
+    });
+
+    it('Should force autoreadyonly if autoselectsingle is true' +
+      'and there\'s only one option', () => {
+      Object.defineProperty(element, 'autoselectsingle', {
+        get () { return this._autoselectsingle; },
+        set (value) { this._autoselectsingle = value; }
+      });
+
+      element.autoselectsingle = true;
+      element.options = ['Seasons'];
+
+      expect(element.autoselectsingle).to.be.true;
+      expect(element.value).to.equal('Seasons');
+    });
+
+    it('Should update the view', () => {
+      element.options = ['Spring', 'Summer', 'Autumn', 'Winter'];
+      expect(element._updateView).to.have.been.calledOnce;
     });
   });
 
   describe('#selection', () => {
-    it('Should', () => {
+    beforeEach(() => {
+      Object.defineProperty(element, 'field', {
+        get () { return this._field; },
+        set (value) { this._field = value; }
+      });
 
+      Object.defineProperty(element, 'parsedOptions', {
+        get () { return this._parsedOptions; },
+        set (value) { this._parsedOptions = value; }
+      });
+    });
+
+    it('Should return the placeholder if the value is undefined', () => {
+      element.field = { value: '' };
+      element.parsedOptions = [
+        { value: 'Spring', content: 'Spring' },
+        { value: 'Summer', content: 'Summer' },
+        { value: 'Autumn', content: 'Autumn' },
+        { value: 'Winter', content: 'Winter' }
+      ];
+      element.placeholder = 'Select season';
+      expect(element.selection).to.equal('Select season');
+    });
+
+    it('Should return the value instead of the placeholder', () => {
+      element.field = { value: 'Spring' };
+      element.parsedOptions = [
+        { value: 'Spring', content: 'Spring' },
+        { value: 'Summer', content: 'Summer' },
+        { value: 'Autumn', content: 'Autumn' },
+        { value: 'Winter', content: 'Winter' }
+      ];
+      element.placeholder = 'Select season';
+      expect(element.selection).to.equal('Spring');
     });
   });
 });
