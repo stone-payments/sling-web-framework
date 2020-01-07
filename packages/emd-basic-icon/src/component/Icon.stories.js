a23b0bf5c5601c2851356a42e6d7c25a97b85484
Leonardo Favre <leo@leofavre.com>
2020-01-07 12:56:10 -0300

    feat: Add Storybook

--- packages/emd-basic-icon/src/component/Icon.stories.js
+++ packages/emd-basic-icon/src/component/Icon.stories.js
@@ -1,5 +1,5 @@
 import { storiesOf } from '@storybook/vue';
-import { withKnobs, number } from '@storybook/addon-knobs';
+import { withKnobs, number, color, boolean } from '@storybook/addon-knobs';
 import readMe from '../../README.md';
 import '../index.js';
 
@@ -10,24 +10,81 @@ const fontOptions = {
   step: 1
 };
 
+const Icon = customElements.get('emd-icon');
+
+const iconNames = Object
+  .keys(Icon.icons)
+  .map(key => key.toLowerCase().replace(/_/g, '-'))
+  .sort();
+
+function getCodeSample (iconName, color) {
+  return `<emd-icon
+  icon="${iconName}${color ? `
+  style="color: ${color}"` : ''}
+></emd-icon>`;
+}
+
+const template = `
+<div
+  class="story"
+  :style="{ fontSize: fontSize + 'px' }"
+>
+  <div class="icongrid">
+    <div v-for="iconName in iconNames" :key="iconName">
+      <emd-icon :icon="iconName" :style="{ color }"></emd-icon>
+      <div v-if="showCodeSamples" class="inlinecodesample" style="margin-top: 20px;">
+        <pre>{{ getCodeSample(iconName, color) }}</pre>
+      </div>
+    </div>
+  </div>
+</div>
+`;
+
 storiesOf('Icon', module)
   .addDecorator(withKnobs)
   .add('Default', () => ({
+    data () {
+      return {
+        iconNames
+      };
+    },
     props: {
       fontSize: {
         default: number('Font size', 16, fontOptions)
+      },
+      showCodeSamples: {
+        default: boolean('Show code samples', true)
+      },
+      color: null
+    },
+    methods: {
+      getCodeSample
+    },
+    template
+  }), {
+    notes: { markdown: readMe }
+  })
+  .add('With Custom Style', () => ({
+    data () {
+      return {
+        iconNames
+      };
+    },
+    props: {
+      fontSize: {
+        default: number('Font size', 16, fontOptions)
+      },
+      color: {
+        default: color('Color', 'black')
+      },
+      showCodeSamples: {
+        default: boolean('Show code samples', true)
       }
     },
-    template: `
-      <div
-        class="story"
-        :style="{ fontSize: fontSize + 'px' }"
-      >
-        <div class="autogrid">
-          <emd-icon icon="close"></emd-icon>
-        </div>
-      </div>
-    `
+    methods: {
+      getCodeSample
+    },
+    template
   }), {
     notes: { markdown: readMe }
   });
