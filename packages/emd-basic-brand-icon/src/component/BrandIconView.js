51e09d47c4e9e941a2497f83b054a861161e65b9
Leonardo Favre <leo@leofavre.com>
2020-01-29 15:45:08 -0300

    feat: Add `nofallback` property so that unknown brands **do not** fallback to Stone

--- packages/emd-basic-brand-icon/src/component/BrandIconView.js
+++ packages/emd-basic-brand-icon/src/component/BrandIconView.js
@@ -1,10 +1,21 @@
 import { html } from '@stone-payments/lit-element';
 
-export const BrandIconView = ({ iconSvg, icon }) => html`
-  <style>
-    @import url("emd-basic-brand-icon/src/component/BrandIcon.css")
-  </style>
-  <div class="emd-brand-icon__wrapper emd-brand-icon__wrapper_${icon}">
-    ${iconSvg}
-  </div>
-`;
+export const BrandIconView = ({
+  iconSvg,
+  icon,
+  stoneIcon,
+  isUnknownIcon
+}) => {
+  let wrapperClass = 'emd-brand-icon__wrapper';
+  wrapperClass += ` emd-brand-icon__wrapper_${icon}`;
+  wrapperClass += isUnknownIcon ? ' emd-brand-icon__wrapper_unknown' : '';
+
+  return html`
+    <style>
+      @import url("emd-basic-brand-icon/src/component/BrandIcon.css")
+    </style>
+    ${!isUnknownIcon ? html`
+      <div class="${wrapperClass}">${iconSvg || stoneIcon}</div>
+    ` : ''}
+  `;
+};
