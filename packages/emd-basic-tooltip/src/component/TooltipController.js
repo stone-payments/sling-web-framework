2c3aac2d819c641603093effcb49faeefc81219a
Leonardo Favre <leo@leofavre.com>
2020-03-18 18:44:20 -0300

    wip: Start implementing `for` property

--- packages/emd-basic-tooltip/src/component/TooltipController.js
+++ packages/emd-basic-tooltip/src/component/TooltipController.js
@@ -13,8 +13,35 @@ export const TooltipController = (Base = class {}) =>
         text: {
           type: String,
           reflect: true
+        },
+        for: {
+          type: String,
+          reflect: true
         }
       };
+    }
+
+    get for () {
+      return this._for;
+    }
+
+    set for (value) {
+      const oldValue = this._for;
+      this._for = value;
+
+      if (this.target) {
+        console.log(this.target);
+      } else {
+        console.log('removed target');
+      }
+
+      this.requestUpdate('for', oldValue);
+    }
+
+    get target () {
+      return this.for && this.parentNode && this.parentNode.children
+        ? Array.from(this.parentNode.children).find(el => el.id === this.for)
+        : undefined;
     }
 
     render () {
