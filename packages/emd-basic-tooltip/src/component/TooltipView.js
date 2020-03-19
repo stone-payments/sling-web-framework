2722085ec32422ecc72b841b05cce7e00f1b188f
Leonardo Favre <leo@leofavre.com>
2020-03-19 12:17:06 -0300

    fix: By using the `for` property the tooltip will occupy space or generate unwanted scrollbars

--- packages/emd-basic-tooltip/src/component/TooltipView.js
+++ packages/emd-basic-tooltip/src/component/TooltipView.js
@@ -1,13 +1,21 @@
 import { html } from '@stone-payments/lit-element';
 
-export const TooltipView = ({ position, text }) => html`
-  <style>
-    @import url("emd-basic-tooltip/src/component/Tooltip.css")
-  </style>
-  <div class="emd-tooltip__wrapper">
-    <slot></slot>
-    <span class="emd-tooltip__text emd-tooltip__text_position_${position || 'right'}">
-      ${text}
-    </span>
-  </div>
-`;
+export const TooltipView = ({ position = 'right', text, target }) => {
+  let wrapperClass = 'emd-tooltip__wrapper';
+  wrapperClass += target ? ' emd-tooltip__wrapper_targeted' : '';
+
+  let tooltipTextClass = 'emd-tooltip__text';
+  tooltipTextClass += ` emd-tooltip__text_position_${position}`;
+
+  return html`
+    <style>
+      @import url("emd-basic-tooltip/src/component/Tooltip.css")
+    </style>
+    <div class="${wrapperClass}">
+      <slot></slot>
+      <span class="${tooltipTextClass}">
+        ${text}
+      </span>
+    </div>
+  `;
+};
