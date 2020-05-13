03ded078e3b573af9ecc0730a072c46a5e0e4ed0
Leonardo Favre <leo@leofavre.com>
2020-05-13 11:10:20 -0300

    feat: Improve the way `icon` property is parsed

--- packages/emd-basic-brand-icon/src/component/BrandIconController.js
+++ packages/emd-basic-brand-icon/src/component/BrandIconController.js
@@ -1,13 +1,4 @@
 export const BrandIconController = (Base = class {}) => class extends Base {
-  static getIconByName (name) {
-    const key = name.normalize('NFD')
-      .replace(/[\u0300-\u036f]/g, '')
-      .replace(/-/g, '_')
-      .replace(' ', '_')
-      .toUpperCase();
-    return this.icons[key] || '';
-  }
-
   static get properties () {
     return {
       icon: {
@@ -21,15 +12,61 @@ export const BrandIconController = (Base = class {}) => class extends Base {
     };
   }
 
-  get iconSvg () {
-    return this.constructor.getIconByName(this.icon);
+  static getPossibleKeys (str) {
+    if (str == null || str === '') {
+      return [];
+    }
+
+    const parsedStr = str
+      .normalize('NFD')
+      .replace(/([\u0300-\u036f]|\.)/g, '')
+      .replace(/-| /g, '_')
+      .toUpperCase();
+
+    return parsedStr
+      .split('_')
+      .reduceRight((result, item, index, arr) => {
+        const firstPossibleKey = arr.slice(0, index + 1).join('_');
+        const secondPossibleKey = firstPossibleKey.replace(/_/g, '');
+        return index !== 0
+          ? [...result, firstPossibleKey, secondPossibleKey]
+          : [...result, firstPossibleKey];
+      }, []);
   }
 
-  get stoneIcon () {
+  static getIconId (name) {
+    const possibleKeys = this.getPossibleKeys(name);
+
+    for (const possibleKey of possibleKeys) {
+      if (this.icons[possibleKey]) {
+        return possibleKey;
+      }
+    }
+
+    return undefined;
+  }
+
+  static getIconView (name) {
+    return this.icons[this.getIconId(name)];
+  }
+
+  get iconId () {
+    return this.constructor.getIconId(this.icon);
+  }
+
+  get iconView () {
+    return this.constructor.getIconView(this.icon);
+  }
+
+  get stoneIconView () {
     return this.constructor.icons.STONE;
   }
 
   get isUnknownIcon () {
-    return this.nofallback && this.iconSvg === '';
+    return this.nofallback && !this.iconView;
+  }
+
+  render () {
+    return this.currentView.apply(this);
   }
 };
--- packages/emd-basic-brand-icon/src/component/BrandIconView.js
+++ packages/emd-basic-brand-icon/src/component/BrandIconView.js
@@ -1,17 +1,24 @@
 import { html } from '@stone-payments/lit-element';
 
 export const BrandIconView = ({
-  iconSvg,
-  icon,
-  stoneIcon,
+  iconView,
+  iconId,
+  stoneIconView,
   isUnknownIcon
-}) => html`
-  <style>
-    @import url("emd-basic-brand-icon/src/component/BrandIcon.css")
-  </style>
-  ${!isUnknownIcon ? html`
-    <div class="emd-brand-icon__wrapper emd-brand-icon__wrapper_${icon}">
-      ${iconSvg || stoneIcon}
-    </div>
-  ` : ''}
-`;
+}) => {
+  let iconClass = iconView
+    ? `emd-brand-icon__${iconId.toLowerCase()}`
+    : 'emd-brand-icon__unknown';
+  iconClass += ' emd-brand-icon__wrapper';
+
+  return html`
+    <style>
+      @import url("emd-basic-brand-icon/src/component/BrandIcon.css")
+    </style>
+    ${!isUnknownIcon ? html`
+      <div class="${iconClass}">
+        ${iconView || stoneIconView}
+      </div>
+    ` : ''}
+  `;
+};
