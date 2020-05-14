cde21361000dc290fdf4a8603e4d01ad5157207f
Leonardo Favre <leo@leofavre.com>
2020-05-14 18:07:16 -0300

    wip: Implement basic PIN behaviour

--- packages/emd-basic-pin-code/src/component/PinCodeController.js
+++ packages/emd-basic-pin-code/src/component/PinCodeController.js
@@ -1,12 +1,59 @@
 export const PinCodeController = (Base = class {}) =>
   class extends Base {
+    constructor () {
+      super();
+      this.handleInput = this.handleInput.bind(this);
+    }
+
     static get properties () {
       return {
-        view: {
+        cases: {
+          type: Number,
+          reflect: true
+        },
+        type: {
           type: String,
           reflect: true
         }
       };
+    }
+
+    attributeChangedCallback (attrName, prevValue, nextValue) {
+      super.attributeChangedCallback(attrName, prevValue, nextValue);
+
+      if (attrName === 'cases') {
+        const numericNextValue = Number(nextValue);
+
+        if (Number.isNaN(numericNextValue) || numericNextValue < 1) {
+          this.setAttribute('cases', 1);
+        } else if (numericNextValue !== Math.round(numericNextValue)) {
+          this.setAttribute('cases', Math.round(numericNextValue));
+        }
+      }
+    }
+
+    get casesArray () {
+      return Array.from(Array(this.cases).keys());
+    }
+
+    get prevToFocus () {
+      const { activeElement } = this.renderRoot;
+      return activeElement.previousElementSibling || activeElement;
+    }
+
+    get nextToFocus () {
+      const { activeElement } = this.renderRoot;
+      return activeElement.nextElementSibling || activeElement;
+    }
+
+    handleInput ({ target }) {
+      target.value = target.value.slice(-1);
+
+      if (target.value === '') {
+        this.prevToFocus.focus();
+      } else {
+        this.nextToFocus.focus();
+      }
     }
 
     render () {
--- packages/emd-basic-pin-code/src/component/PinCodeView.js
+++ packages/emd-basic-pin-code/src/component/PinCodeView.js
@@ -1,10 +1,20 @@
 import { html } from '@stone-payments/lit-element';
 
-export const PinCodeView = () => html`
+export const PinCodeView = ({
+  type = 'text',
+  casesArray,
+  handleInput
+}) => html`
   <style>
     @import url("emd-basic-pin-code/src/component/PinCode.css")
   </style>
-  <div>
-    <p>This is your new Emerald web component.</p>
+  <div class="emd-pin-code__wrapper">
+    ${casesArray.map(item => html`
+      <input
+        @input="${handleInput}"
+        data-case="${item}"
+        type="${type}"
+      />
+    `)}
   </div>
 `;
