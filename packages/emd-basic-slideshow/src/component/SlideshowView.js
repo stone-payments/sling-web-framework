f68ede5033e4c5a2628dc0f8b2cbd77813505f00
Leonardo Favre <leo@leofavre.com>
2020-07-03 17:38:05 -0300

    chore: Implement the View

--- packages/emd-basic-slideshow/src/component/SlideshowView.js
+++ packages/emd-basic-slideshow/src/component/SlideshowView.js
@@ -4,7 +4,9 @@ export const SlideshowView = () => html`
   <style>
     @import url("emd-basic-slideshow/src/component/Slideshow.css")
   </style>
-  <div>
-    <p>This is your new Emerald web component.</p>
+  <div class="emd-slideshow__wrapper">
+    <div class="container">
+      <slot></slot>
+    </div>
   </div>
 `;
