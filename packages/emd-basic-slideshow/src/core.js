1d7aaed5684d89c9a59370758aac5677f1c1018d
Leonardo Favre <leo@leofavre.com>
2020-07-03 16:44:53 -0300

    chore: Use withObservedChildren

--- packages/emd-basic-slideshow/src/core.js
+++ packages/emd-basic-slideshow/src/core.js
@@ -1,5 +1,5 @@
 import { compose } from '@stone-payments/emd-helpers';
-import { withComponent } from '@stone-payments/emd-hocs';
+import { withComponent, withObservedChildren } from '@stone-payments/emd-hocs';
 import { LitElement } from '@stone-payments/lit-element';
 
 import { SlideshowController } from './component/SlideshowController.js';
@@ -7,6 +7,7 @@ import { SlideshowView } from './component/SlideshowView.js';
 
 const Slideshow = compose(
   SlideshowController,
+  withObservedChildren,
   withComponent
 )(LitElement);
 
